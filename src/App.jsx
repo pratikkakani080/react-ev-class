c33f62bf8f451c17853e41d7fe6ddce90dfbab3e
Your Name <you@example.com>
2025-08-26 17:27:10 +0530

    data filterations

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,41 @@ import Form from './modules/form';
 // useEffect
 // useContext
 
-function App() {
-  // const [count, setCount] = useState(0)
-  // console.log('outside logger statement');
+const data = [1, 3, 5, 7, 8 ,5, 3, 4, 6, 4]
 
-  // useEffect(() => {
-  //   console.log('this component mounted __ inside logger statement');
-  //   return () => {
-  //     console.log('this component getting unmounted');
-  //   }
-  // }, [count])
+function App() {
+  const [count, setCount] = useState(0)
+  const [filteredData, setFilteredData] = useState(data)
+  let aa = 0
+  console.log('outside logger statement');
+
+  useEffect(() => {
+    console.log('this component mounted __ inside logger statement');
+
+    // clean up function
+    return () => {
+      console.log('this component getting unmounted');
+    }
+  }, [count])
 
   return (
     <>
-      <Form />
-      {/* <Button buttonText='Cancel' bgColor='red' onClick={() => setCount(count + 1)} /> */}
-      {/* {count} */}
+      {/* <Form /> */}
+      {aa}
+      <Button buttonText='Cancel' bgColor='red' onClick={() => {
+        setCount(count + 1); aa = aa + 1; console.log(aa);
+      }} />
+      {count}
+      <input type='search' onChange={(e) => {
+        let searchStr = e.target.value
+        setFilteredData(searchStr.length === 0 ? data : data.filter(el => String(el) === searchStr))
+      }} />
+
+      {
+        filteredData.map(el => {
+          return <p>{el}</p>
+        })
+      }
     </>
   )
 }
